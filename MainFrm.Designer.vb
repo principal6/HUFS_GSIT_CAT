ed70ca7b48b6bc15c8480f79b3270f95e0b49a62
JesusKim <jesuskim666@gmail.com>
2018-12-28 18:03:28 +0900

    Initial commit #2

---
+++ MainFrm.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MainFrm
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.CMS1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.검색ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.닫기ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TBWord = New System.Windows.Forms.TextBox()
+        Me.TBST = New System.Windows.Forms.TextBox()
+        Me.TBSTSentence = New System.Windows.Forms.TextBox()
+        Me.TBTTSentence = New System.Windows.Forms.TextBox()
+        Me.TBTT = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.LBCurSentence = New System.Windows.Forms.Label()
+        Me.CMS1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'CMS1
+        '
+        Me.CMS1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.검색ToolStripMenuItem, Me.닫기ToolStripMenuItem})
+        Me.CMS1.Name = "CMS1"
+        Me.CMS1.Size = New System.Drawing.Size(99, 48)
+        '
+        '검색ToolStripMenuItem
+        '
+        Me.검색ToolStripMenuItem.Name = "검색ToolStripMenuItem"
+        Me.검색ToolStripMenuItem.Size = New System.Drawing.Size(98, 22)
+        Me.검색ToolStripMenuItem.Text = "검색"
+        '
+        '닫기ToolStripMenuItem
+        '
+        Me.닫기ToolStripMenuItem.Name = "닫기ToolStripMenuItem"
+        Me.닫기ToolStripMenuItem.Size = New System.Drawing.Size(98, 22)
+        Me.닫기ToolStripMenuItem.Text = "닫기"
+        '
+        'TBWord
+        '
+        Me.TBWord.Location = New System.Drawing.Point(12, 160)
+        Me.TBWord.Name = "TBWord"
+        Me.TBWord.Size = New System.Drawing.Size(138, 21)
+        Me.TBWord.TabIndex = 2
+        '
+        'TBST
+        '
+        Me.TBST.BackColor = System.Drawing.Color.Honeydew
+        Me.TBST.Location = New System.Drawing.Point(172, 160)
+        Me.TBST.Multiline = True
+        Me.TBST.Name = "TBST"
+        Me.TBST.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TBST.Size = New System.Drawing.Size(404, 423)
+        Me.TBST.TabIndex = 0
+        '
+        'TBSTSentence
+        '
+        Me.TBSTSentence.BackColor = System.Drawing.Color.LightGreen
+        Me.TBSTSentence.Location = New System.Drawing.Point(172, 12)
+        Me.TBSTSentence.Multiline = True
+        Me.TBSTSentence.Name = "TBSTSentence"
+        Me.TBSTSentence.ReadOnly = True
+        Me.TBSTSentence.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TBSTSentence.Size = New System.Drawing.Size(404, 142)
+        Me.TBSTSentence.TabIndex = 3
+        '
+        'TBTTSentence
+        '
+        Me.TBTTSentence.BackColor = System.Drawing.Color.LightGreen
+        Me.TBTTSentence.Location = New System.Drawing.Point(592, 12)
+        Me.TBTTSentence.Multiline = True
+        Me.TBTTSentence.Name = "TBTTSentence"
+        Me.TBTTSentence.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TBTTSentence.Size = New System.Drawing.Size(404, 142)
+        Me.TBTTSentence.TabIndex = 5
+        '
+        'TBTT
+        '
+        Me.TBTT.BackColor = System.Drawing.Color.Honeydew
+        Me.TBTT.Location = New System.Drawing.Point(592, 160)
+        Me.TBTT.Multiline = True
+        Me.TBTT.Name = "TBTT"
+        Me.TBTT.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TBTT.Size = New System.Drawing.Size(404, 423)
+        Me.TBTT.TabIndex = 6
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.LBCurSentence)
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(154, 142)
+        Me.Panel1.TabIndex = 7
+        '
+        'LBCurSentence
+        '
+        Me.LBCurSentence.AutoSize = True
+        Me.LBCurSentence.Location = New System.Drawing.Point(3, 2)
+        Me.LBCurSentence.Name = "LBCurSentence"
+        Me.LBCurSentence.Size = New System.Drawing.Size(61, 12)
+        Me.LBCurSentence.TabIndex = 0
+        Me.LBCurSentence.Text = "현재 문장:"
+        '
+        'MainFrm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1008, 606)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.TBTT)
+        Me.Controls.Add(Me.TBTTSentence)
+        Me.Controls.Add(Me.TBSTSentence)
+        Me.Controls.Add(Me.TBST)
+        Me.Controls.Add(Me.TBWord)
+        Me.Name = "MainFrm"
+        Me.Text = "HUFS GSIT CAT"
+        Me.CMS1.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents CMS1 As ContextMenuStrip
+    Friend WithEvents 검색ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents 닫기ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TBWord As TextBox
+    Friend WithEvents TBST As TextBox
+    Friend WithEvents TBSTSentence As TextBox
+    Friend WithEvents TBTTSentence As TextBox
+    Friend WithEvents TBTT As TextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents LBCurSentence As Label
+End Class
---
+++ MainFrm.vb
@@ -0,0 +1,173 @@
+﻿Imports System.ComponentModel
+
+Public Class MainFrm
+    Dim g_FontMain As New Font("맑은 고딕", 12, FontStyle.Regular)
+    Dim g_FontSentence As New Font("맑은 고딕", 11, FontStyle.Regular)
+    Dim g_SentenceID() As Long
+    Dim g_NumSentences As Long
+    Dim g_sTransSentence(1000) As String    '총 1000 문장
+    Dim g_nCurSentID As Long
+
+    Dim g_PrevSTCursor As Long
+
+    Public Sub UpdateTT()
+        TBTT.Text = Nothing
+
+        If g_NumSentences = 0 Then Exit Sub
+
+        For i = 0 To g_NumSentences - 1
+            TBTT.Text = TBTT.Text & g_sTransSentence(i) & vbCrLf
+        Next
+    End Sub
+
+    Public Sub DevideSentences(ByVal sText As String)
+
+        If sText.Length = 0 Then Exit Sub
+
+        g_NumSentences = 0
+        For i = 1 To sText.Length
+            If Mid(sText, i, 1) = "." Or Mid(sText, i, 2) = vbCrLf Then
+                g_NumSentences = g_NumSentences + 1
+            End If
+        Next
+
+        ReDim g_SentenceID(g_NumSentences)
+
+        Dim nTempNumSentences As Long = 0
+        For i = 1 To sText.Length
+            If Mid(sText, i, 1) = "." Then
+                nTempNumSentences = nTempNumSentences + 1
+                g_SentenceID(nTempNumSentences - 1) = i
+            ElseIf Mid(sText, i, 2) = vbCrLf Then
+                nTempNumSentences = nTempNumSentences + 1
+                g_SentenceID(nTempNumSentences - 1) = i + 1
+            End If
+        Next
+
+    End Sub
+
+    Public Function GetSentenceID(ByVal CurSelStart As Long) As Long
+        GetSentenceID = 0
+
+        For i = 0 To g_NumSentences
+            If CurSelStart <= g_SentenceID(i) Then
+                GetSentenceID = i
+                Exit For
+            End If
+        Next
+    End Function
+
+    Public Function GetSentenceText(ByVal sText As String, ByVal SentenceID As Long) As String
+        GetSentenceText = Nothing
+
+        If SentenceID = 0 Then
+            GetSentenceText = Trim(Mid(sText, 1, g_SentenceID(SentenceID)))
+        Else
+            GetSentenceText = Trim(Mid(sText, g_SentenceID(SentenceID - 1) + 1, g_SentenceID(SentenceID) - g_SentenceID(SentenceID - 1)))
+        End If
+
+    End Function
+
+    Public Function FindWordInText(ByVal sText As String, ByVal SelStart As Long) As String
+        FindWordInText = Nothing
+
+        If sText.Length = 0 Then Exit Function
+
+        Dim nWordSelStart As Long
+        Dim nWordSelEnd As Long
+        Dim nWordSelLen As Long
+
+        If SelStart = 0 Then
+            nWordSelStart = 1
+        Else
+            nWordSelStart = 1
+            For i = SelStart To 1 Step -1
+                If Mid(sText, i, 1) = " " Or Mid(sText, i, 1) = vbTab Or Mid(sText, i, 1) = vbLf Then
+                    nWordSelStart = i + 1
+                    Exit For
+                End If
+            Next
+        End If
+
+        nWordSelEnd = sText.Length + 1
+        For i = SelStart + 1 To sText.Length
+            If Mid(sText, i, 1) = " " Or Mid(sText, i, 1) = vbTab Or Mid(sText, i, 1) = vbLf Then
+                nWordSelEnd = i
+                Exit For
+            End If
+        Next
+
+        nWordSelLen = nWordSelEnd - nWordSelStart
+        If nWordSelLen < 0 Then nWordSelLen = 0
+
+        FindWordInText = Mid(sText, nWordSelStart, nWordSelLen)
+
+    End Function
+
+    Public Sub FindWordAndSentences()
+        g_PrevSTCursor = TBST.SelectionStart
+
+        TBWord.Text = FindWordInText(TBST.Text, g_PrevSTCursor)
+        g_nCurSentID = GetSentenceID(g_PrevSTCursor)
+        TBSTSentence.Text = GetSentenceText(TBST.Text, g_nCurSentID)
+
+        LBCurSentence.Text = "현재 문장: " & g_nCurSentID + 1 & " / " & g_NumSentences
+
+        TBTTSentence.Text = g_sTransSentence(g_nCurSentID)
+    End Sub
+
+    Private Sub MainFrm_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        End
+    End Sub
+
+    Private Sub MainFrm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        TBST.Font = g_FontMain
+        TBST.ContextMenuStrip = CMS1
+        TBTT.Font = g_FontMain
+
+        TBSTSentence.Font = g_FontSentence
+        TBTTSentence.Font = g_FontSentence
+    End Sub
+
+    Private Sub TBST_KeyUp(sender As Object, e As KeyEventArgs) Handles TBST.KeyUp
+        If TBST.Text.Length = 0 Then Exit Sub
+
+        FindWordAndSentences()
+
+        If e.KeyCode = Keys.F1 Then
+            TBTTSentence.Focus()
+        End If
+    End Sub
+    Private Sub TBST_MouseUp(sender As Object, e As MouseEventArgs) Handles TBST.MouseUp
+        If TBST.Text.Length = 0 Then Exit Sub
+
+        FindWordAndSentences()
+    End Sub
+
+    Private Sub TBST_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TBST.KeyPress
+        If e.KeyChar = Convert.ToChar(1) Then
+            TBST.SelectAll()
+        End If
+    End Sub
+
+    Private Sub TBST_TextChanged(sender As Object, e As EventArgs) Handles TBST.TextChanged
+        DevideSentences(TBST.Text)
+    End Sub
+
+    Private Sub TBTTSentence_KeyDown(sender As Object, e As KeyEventArgs) Handles TBTTSentence.KeyDown
+        If e.KeyCode = Keys.Enter Then
+            g_sTransSentence(g_nCurSentID) = TBTTSentence.Text
+            e.SuppressKeyPress = True
+
+            UpdateTT()
+
+            TBST.Focus()
+            TBST.SelectionStart = g_PrevSTCursor
+        ElseIf e.KeyCode = Keys.Escape Then
+            TBST.Focus()
+            TBST.SelectionStart = g_PrevSTCursor
+        End If
+    End Sub
+
+End Class
+
